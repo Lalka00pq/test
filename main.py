59d08b10c60f13e6065f2e1f3842b50f55f80705
Lalka00pq <lalka00pq@gmail.com>
2025-01-27 14:07:38 +0300

    Init

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import Callable
+
+
+def my_decorator(func: Callable) -> Callable:
+    def wrapper():
+        a = func()
+        a = a[:4]
+        print(a)
+    return wrapper
+
+@my_decorator
+def say_hello():
+    return 'Hello World'
+
+say_hello()
